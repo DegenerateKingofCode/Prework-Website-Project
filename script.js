4d3493472423d5ed6b1cfaa90479484eaee121b0
Tanner <Tanner@tleroy.com>
2022-03-13 16:02:41 -0400

    edited html format and css, tried to add event listener

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ h2.textContent = "Tanner Leroy: A Webpage Story"
 
 document.querySelector("body").appendChild(h2);
 
-let link = document.getElementById("link");
-link.addEventListener("mouseover", function(event){
-    event.target.style.color = "orange"
+const link = document.getElementById("link");
+link.addEventListener("click", function(){
+    alert('Swagger Alert!') 
 })
