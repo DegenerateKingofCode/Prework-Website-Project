705f28f440b28035406eec6fc0d25743547a63c5
Tanner <Tanner@tleroy.com>
2022-03-13 15:51:27 -0400

    added link and edited css

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const h2 = document.createElement("h2");
 h2.textContent = "Tanner Leroy: A Webpage Story"
 
 document.querySelector("body").appendChild(h2);
+
+let link = document.getElementById("link");
+link.addEventListener("mouseover", function(event){
+    event.target.style.color = "orange"
+})
