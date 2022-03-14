232494896f14e5d2c403d129589b3595fc3f566b
Tanner <Tanner@tleroy.com>
2022-03-13 20:57:39 -0400

    Final Changes before eventsListener is added

--- script.js
+++ script.js
@@ -3,7 +3,5 @@ h2.textContent = "Tanner Leroy: A Webpage Story"
 
 document.querySelector("body").appendChild(h2);
 
-const imageDescription = document.getElementsByClassName("imageDescription");
-imageDescription.addEventListener("click", function() {
-    alert('Swagger Alert!') 
-})
+
+
