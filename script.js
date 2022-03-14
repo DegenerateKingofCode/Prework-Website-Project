bd04f5aa5f0e0b9667282762dd217b13ac32b273
Tanner <Tanner@tleroy.com>
2022-03-14 10:21:18 -0400

    Final Edits, got an event listener working

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ h2.textContent = "Tanner Leroy: A Webpage Story"
 document.querySelector("body").appendChild(h2);
 
 
+let inputBox = document.getElementById("inputBox")
+inputBox.addEventListener('mouseover', inputWrite)
 
+function inputWrite(e){
+    alert("This Input will eventually work?")
+}
