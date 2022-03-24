b7ee3910400a3474d3ed93b829f3cd23201c90a9
Tanner <Tanner@tleroy.com>
2022-03-24 13:34:25 -0400

    Final Changes to Website and text location

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const h2 = document.createElement("h2");
-h2.textContent = "Tanner Leroy: A Webpage Story"
+const h3 = document.createElement("h3");
+h3.textContent = "Let's see how this works out."
 
-document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h3);
 
 
 let inputBox = document.getElementById("inputBox")
